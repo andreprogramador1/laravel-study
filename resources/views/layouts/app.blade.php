314f5eec2049b68850b47de23572eea69633b563
andre <andre_epr@outlook.com>
2021-01-20 13:48:50 -0300

    tailwind started and some styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-<body>
-    
+<body class='bg-gray-200'>
+    <nav class='p-6 bg-white flex justify-between mb-6'>
+        <ul class='flex items-center'>
+            <li class='p-3'>
+                <a href="">André Ponce</a>
+            </li>
+            <li class='p-3'>
+                <a href="">Login</a>
+            </li>
+            <li class='p-3'>
+                <a href="">Register</a>
+            </li>
+            <li class='p-3'>
+                <a href="">Logout</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
